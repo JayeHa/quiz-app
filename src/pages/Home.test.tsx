b69559029776ffa85134f8a2e5b7850c65346a88
정원 <jwha94@gmail.com>
2024-02-22 01:03:59 +0900

    test: 'Home' 컴포넌트에 대한 라우팅 테스트 추가

--- src/pages/Home.test.tsx
+++ src/pages/Home.test.tsx
@@ -1,12 +1,32 @@
 import "@testing-library/jest-dom";
 import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { Route } from "react-router-dom";
+import { withRouter } from "../tests/utils";
 import { Home } from "./Home";
 
 describe("Home", () => {
-  it("should render '퀴즈 풀기' button", () => {
-    render(<Home />);
+  it("'퀴즈 풀기' 버튼이 렌더링된다.", () => {
+    render(withRouter(<Route path="/" element={<Home />} />));
     const startButton = screen.getByRole("button", { name: /퀴즈 풀기/ });
 
     expect(startButton).toBeInTheDocument();
+  });
+
+  it("사용자는 '퀴즈 풀기' 버튼을 클릭하여 퀴즈 풀기 페이지로 이동된다.", () => {
+    const QUIZ_PAGE_TEXT = "퀴즈 풀기 페이지";
+    render(
+      withRouter(
+        <>
+          <Route path="/" element={<Home />} />
+          <Route path="/quiz" element={<div>{QUIZ_PAGE_TEXT}</div>} />
+        </>
+      )
+    );
+
+    const startButton = screen.getByRole("button", { name: /퀴즈 풀기/ });
+    userEvent.click(startButton);
+
+    expect(screen.getByText(QUIZ_PAGE_TEXT)).toBeInTheDocument();
   });
 });
