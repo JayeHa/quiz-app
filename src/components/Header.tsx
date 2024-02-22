83580346acabad1770898f906fb1b267e8788cc3
정원 <jwha94@gmail.com>
2024-02-23 04:35:18 +0900

    chore: Add custom color to tailwind.config

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,7 +4,7 @@ export const Header = () => {
   return (
     <header className="flex h-20 items-center justify-center shadow">
       <Link to="/">
-        <h1 className="text-4xl font-bold tracking-tighter text-red-800">
+        <h1 className="text-red hover:text-red-dark text-4xl font-bold tracking-tighter transition-colors">
           QuizApp
         </h1>
       </Link>
