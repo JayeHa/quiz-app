a61c2e7487a78234b0250aa1f498af9d7bea3465
정원 <jwha94@gmail.com>
2024-02-23 03:58:33 +0900

    feat: "prettier-plugin-tailwindcss" 추가

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export const Header = () => {
   return (
-    <header className="shadow h-20 flex items-center justify-center">
+    <header className="flex h-20 items-center justify-center shadow">
       <Link to="/">
         <h1 className="text-4xl font-bold tracking-tighter text-red-800">
           QuizApp
