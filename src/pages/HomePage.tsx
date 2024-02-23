cd0b53a0f04e6fc5a92336971f4fb26995b54b05
정원 <jwha94@gmail.com>
2024-02-23 19:49:53 +0900

    style(HomePage): Home 페이지 스타일링 작업

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,28 @@
 import { Button } from "@components/Button";
+import { Heading } from "@components/Heading";
+import { DEFAULT_AMOUNT } from "@model/quiz";
 import { useNavigate } from "react-router-dom";
 
 export const HomePage = () => {
   const navigate = useNavigate();
 
   return (
-    <div>
-      <Button onClick={() => navigate("/quiz")}>퀴즈 풀기</Button>
+    <div className="flex flex-col items-center gap-16">
+      <div>
+        <Heading level={2} className="break-keep text-center">
+          퀴즈앱에 오신 것을 환영합니다!
+        </Heading>
+
+        <p className="text-center text-xl">
+          아래 버튼을 눌러 퀴즈를 시작해주세요.
+          <br />
+          문제는 총 {DEFAULT_AMOUNT}개가 제공됩니다.
+        </p>
+      </div>
+
+      <Button onClick={() => navigate("/quiz")} className="w-full md:w-[500px]">
+        퀴즈 풀기
+      </Button>
     </div>
   );
 };
