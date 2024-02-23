283b9a6d4622a0331fb0067f805192e4d3c05983
정원 <jwha94@gmail.com>
2024-02-23 16:34:04 +0900

    chore(QuestionBox): prefix뒤에 공백 추가

--- src/components/QuizCards/QuestionBox.tsx
+++ src/components/QuizCards/QuestionBox.tsx
@@ -20,8 +20,7 @@ export const QuestionBox = ({
     <div>
       <Text className="text-lg font-semibold text-neutral-500">{category}</Text>
       <Heading level={3} size="md">
-        <span>{prefix}</span>
-        <span>{index + 1}. </span>
+        <span>{prefix} </span> <span>{index + 1}. </span>
         <Text>{question}</Text>{" "}
         <Text className="text-xl font-semibold text-green">{`[${difficulty}]`}</Text>
       </Heading>
