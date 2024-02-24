65fe98689b05aa5d1fbbbab39077da9bd9de30a1
정원 <jwha94@gmail.com>
2024-02-24 16:30:48 +0900

    refactor: SkeletonQuizProgress 컴포넌트를 별도의 모듈로 분리

--- src/components/QuizCards/QuizCard/QuizCard.skeleton.tsx
+++ src/components/QuizCards/QuizCard/QuizCard.skeleton.tsx
@@ -1,15 +1,5 @@
 import Skeleton from "@components/Skeleton";
-
-export const SkeletonQuizProgress = () => {
-  return (
-    <Skeleton.Container>
-      <div className="flex h-8 flex-col gap-1 bg-white">
-        <div className="w-14 flex-1 rounded-lg bg-skeleton" />
-        <div className="h-1 bg-skeleton" />
-      </div>
-    </Skeleton.Container>
-  );
-};
+import { SkeletonQuizProgress } from "../QuizProgess.skeleton";
 
 export const SkeletonQuizCard = () => {
   return (
